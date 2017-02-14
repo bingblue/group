f8afd7a001a3109fc43666075f482e55ea5a4f11
XiaoMu <895355044@qq.com>
2017-02-14 10:44:58 +0800

    add fere

--- routes/fere.js
+++ routes/fere.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const path = require('path');
 
 //过滤器
 router.all('*',(req, res, next)=>{
@@ -7,7 +8,8 @@ router.all('*',(req, res, next)=>{
 
 //
 router.get('/', (req, res)=>{
-  res.sendfile('views/fere/goto.html');
+  res.sendfile(path.join(__dirname, '../views/fere/goto.html'));
+  //res.sendfile('views/fere/goto.html');
 });
 
 
