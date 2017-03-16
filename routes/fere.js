25fa2dad96825e998311e22b9c5f657b6e839cc4
xiaomucool-home <a59769507@hotmail.com>
2017-03-16 17:12:49 +0800

    update

--- routes/fere.js
+++ routes/fere.js
@@ -10,263 +10,263 @@ router.get('/', (req, res)=>{
   res.sendfile('views/fere/home.html');
 });
 //
-router.get('/chat', (req, res)=>{
+router.get('/chat.html', (req, res)=>{
   res.sendfile('views/fere/chat.html');
 });
 //
-router.get('/follow', (req, res)=>{
+router.get('/follow.html', (req, res)=>{
   res.sendfile('views/fere/follow.html');
 });
 //
-router.get('/follow-list', (req, res)=>{
+router.get('/follow-list.html', (req, res)=>{
   res.sendfile('views/fere/follow-list.html');
 });
 //
-router.get('/history', (req, res)=>{
+router.get('/history.html', (req, res)=>{
   res.sendfile('views/fere/history.html');
 });
 //
-router.get('/history-list', (req, res)=>{
+router.get('/history-list.html', (req, res)=>{
   res.sendfile('views/fere/history-list.html');
 });
 //
-router.get('/land', (req, res)=>{
+router.get('/land.html', (req, res)=>{
   res.sendfile('views/fere/land.html');
 });
 //
-router.get('/menu', (req, res)=>{
+router.get('/menu.html', (req, res)=>{
   res.sendfile('views/fere/menu.html');
 });
 //
-router.get('/personal', (req, res)=>{
+router.get('/personal.html', (req, res)=>{
   res.sendfile('views/fere/personal.html');
 });
 //
-router.get('/personal-edit', (req, res)=>{
+router.get('/personal-edit.html', (req, res)=>{
   res.sendfile('views/fere/personal-edit.html');
 });
 //
-router.get('/personal-gift', (req, res)=>{
+router.get('/personal-gift.html', (req, res)=>{
   res.sendfile('views/fere/personal-gift.html');
 });
 //
-router.get('/personal-info', (req, res)=>{
+router.get('/personal-info.html', (req, res)=>{
   res.sendfile('views/fere/personal-info.html');
 });
 //
-router.get('/personal-info-edit', (req, res)=>{
+router.get('/personal-info-edit.html', (req, res)=>{
   res.sendfile('views/fere/personal-info-edit.html');
 });
 //
-router.get('/personal-market', (req, res)=>{
+router.get('/personal-market.html', (req, res)=>{
   res.sendfile('views/fere/personal-market.html');
 });
 //
-router.get('/personal-message', (req, res)=>{
+router.get('/personal-message.html', (req, res)=>{
   res.sendfile('views/fere/personal-message.html');
 });
 //
-router.get('/share-comment', (req, res)=>{
+router.get('/share-comment.html', (req, res)=>{
   res.sendfile('views/fere/share-comment.html');
 });
 //
-router.get('/share-photo', (req, res)=>{
+router.get('/share-photo.html', (req, res)=>{
   res.sendfile('views/fere/share-photo.html');
 });
 //
-router.get('/share-up', (req, res)=>{
+router.get('/share-up.html', (req, res)=>{
   res.sendfile('views/fere/share-up.html');
 });
 //
-router.get('/Sign-in', (req, res)=>{
+router.get('/Sign-in.html', (req, res)=>{
   res.sendfile('views/fere/Sign-in.html');
 });
 //
-router.get('/Sign-in2', (req, res)=>{
+router.get('/Sign-in2.html', (req, res)=>{
   res.sendfile('views/fere/Sign-in2.html');
 });
 //
-router.get('/Sign-in3', (req, res)=>{
+router.get('/Sign-in3.html', (req, res)=>{
   res.sendfile('views/fere/Sign-in3.html');
 });
 //
-router.get('/Sign-in4', (req, res)=>{
+router.get('/Sign-in4.html', (req, res)=>{
   res.sendfile('views/fere/Sign-in4.html');
 });
 //
-router.get('/Sign-in5', (req, res)=>{
+router.get('/Sign-in5.html', (req, res)=>{
   res.sendfile('views/fere/Sign-in5.html');
 });
 //
-router.get('/start', (req, res)=>{
+router.get('/start.html', (req, res)=>{
   res.sendfile('views/fere/start.html');
 });
 //
-router.get('/start1', (req, res)=>{
+router.get('/start1.html', (req, res)=>{
   res.sendfile('views/fere/start1.html');
 });
 //
-router.get('/start2', (req, res)=>{
+router.get('/start2.html', (req, res)=>{
   res.sendfile('views/fere/start2.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 //
-router.get('/demo', (req, res)=>{
+router.get('/demo.html', (req, res)=>{
   res.sendfile('views/fere/demo.html');
 });
 
