29e2b3f82860f19cd872af9f61f98040fb1afd20
xiaomucool-home <a59769507@hotmail.com>
2017-03-16 16:27:13 +0800

    update fere url

--- routes/fere.js
+++ routes/fere.js
@@ -9,6 +9,266 @@ router.all('*',(req, res, next)=>{
 router.get('/', (req, res)=>{
   res.sendfile('views/fere/home.html');
 });
+//
+router.get('/chat', (req, res)=>{
+  res.sendfile('views/fere/chat.html');
+});
+//
+router.get('/follow', (req, res)=>{
+  res.sendfile('views/fere/follow.html');
+});
+//
+router.get('/follow-list', (req, res)=>{
+  res.sendfile('views/fere/follow-list.html');
+});
+//
+router.get('/history', (req, res)=>{
+  res.sendfile('views/fere/history.html');
+});
+//
+router.get('/history-list', (req, res)=>{
+  res.sendfile('views/fere/history-list.html');
+});
+//
+router.get('/land', (req, res)=>{
+  res.sendfile('views/fere/land.html');
+});
+//
+router.get('/menu', (req, res)=>{
+  res.sendfile('views/fere/menu.html');
+});
+//
+router.get('/personal', (req, res)=>{
+  res.sendfile('views/fere/personal.html');
+});
+//
+router.get('/personal-edit', (req, res)=>{
+  res.sendfile('views/fere/personal-edit.html');
+});
+//
+router.get('/personal-gift', (req, res)=>{
+  res.sendfile('views/fere/personal-gift.html');
+});
+//
+router.get('/personal-info', (req, res)=>{
+  res.sendfile('views/fere/personal-info.html');
+});
+//
+router.get('/personal-info-edit', (req, res)=>{
+  res.sendfile('views/fere/personal-info-edit.html');
+});
+//
+router.get('/personal-market', (req, res)=>{
+  res.sendfile('views/fere/personal-market.html');
+});
+//
+router.get('/personal-message', (req, res)=>{
+  res.sendfile('views/fere/personal-message.html');
+});
+//
+router.get('/share-comment', (req, res)=>{
+  res.sendfile('views/fere/share-comment.html');
+});
+//
+router.get('/share-photo', (req, res)=>{
+  res.sendfile('views/fere/share-photo.html');
+});
+//
+router.get('/share-up', (req, res)=>{
+  res.sendfile('views/fere/share-up.html');
+});
+//
+router.get('/Sign-in', (req, res)=>{
+  res.sendfile('views/fere/Sign-in.html');
+});
+//
+router.get('/Sign-in2', (req, res)=>{
+  res.sendfile('views/fere/Sign-in2.html');
+});
+//
+router.get('/Sign-in3', (req, res)=>{
+  res.sendfile('views/fere/Sign-in3.html');
+});
+//
+router.get('/Sign-in4', (req, res)=>{
+  res.sendfile('views/fere/Sign-in4.html');
+});
+//
+router.get('/Sign-in5', (req, res)=>{
+  res.sendfile('views/fere/Sign-in5.html');
+});
+//
+router.get('/start', (req, res)=>{
+  res.sendfile('views/fere/start.html');
+});
+//
+router.get('/start1', (req, res)=>{
+  res.sendfile('views/fere/start1.html');
+});
+//
+router.get('/start2', (req, res)=>{
+  res.sendfile('views/fere/start2.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
+//
+router.get('/demo', (req, res)=>{
+  res.sendfile('views/fere/demo.html');
+});
 
 
 module.exports = router;
