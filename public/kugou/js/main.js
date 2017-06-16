00848efe15a18c7a8fbdc3317fe6d1a33295c37a
XiaoMuCOOL <895355044@qq.com>
2017-06-16 17:14:15 +0800

    update kugou

--- public/kugou/js/main.js
+++ public/kugou/js/main.js
@@ -1,22 +1,25 @@
 $(function(){
   var video = $("#myvideo")[0];
-  video.addEventListener("loadeddata",function(){
-    console.log("loadeddata下载完成！");
+  // video.addEventListener("loadeddata",function(){
+  //   console.log("loadeddata下载完成！");
+  //   $(".mask1").fadeIn(200);
+  // });
+  // video.addEventListener("canplay",function(){
+  //   console.log("canplay下载完成！");
+  //   $(".mask1").fadeIn(200);
+  // });
+  // video.addEventListener("play",function(){
+  //   console.log("play下载完成！");
+  //   $(".mask1").fadeIn(200);
+  // });
+  // $("#myvideo").load(function(){
+  //   console.log("onload下载完成！");
+  //   alert('document');
+  //   $(".mask1").fadeIn(200);
+  // });
+  setTimeout(function(){
     $(".mask1").fadeIn(200);
-  });
-  video.addEventListener("canplay",function(){
-    console.log("canplay下载完成！");
-    $(".mask1").fadeIn(200);
-  });
-  video.addEventListener("play",function(){
-    console.log("play下载完成！");
-    $(".mask1").fadeIn(200);
-  });
-  document.onload = function(){
-    console.log("onload下载完成！");
-    alert('document');
-    $(".mask1").fadeIn(200);
-  };
+  },3000);
   $(".star").click(function(){
     $(".wrap1").hide();
     $("#myvideo").show();
