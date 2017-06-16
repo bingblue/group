d878e98c33cc51740e445db747baa9e63733c703
XiaoMuCOOL <895355044@qq.com>
2017-06-16 16:50:18 +0800

    update kugou

--- public/kugou/js/main.js
+++ public/kugou/js/main.js
@@ -1,9 +1,9 @@
 $(function(){
   var video = $("#myvideo")[0];
-  video.addEventListener("canplay",function(){
+  video.addEventListener("loadeddata",function(){
     console.log("下载完成！");
-  });
     $(".mask1").fadeIn(200);
+  });
   $(".star").click(function(){
     $(".wrap1").hide();
     $("#myvideo").show();
